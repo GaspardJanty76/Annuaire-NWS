be2589c368c686d54633b034740eff953949fc4c
Gaspard Janty <144214357+GaspardJanty76@users.noreply.github.com>
2023-10-22 14:54:42 +0200

    V3

--- methodes/update.php
+++ methodes/update.php
@@ -1,31 +1,37 @@
 <?php
-if (isset($_POST['modifier'])) {
-    include('connexion.php');
-    $connexion = connexionBdd();
+include_once __DIR__ . '/../templates/header.php';
+function modifierEleves() {
+    if (isset($_POST['modifier'])) {
+        include('connexion.php');
+        $connexion = connexionBdd();
 
-    $count = count($_POST['ideleves']);
+        $count = count($_POST['ideleves']);
 
-    for ($i = 0; $i < $count; $i++) {
-        $ideleves = $_POST['ideleves'][$i];
-        $nom = $_POST['nom'][$i];
-        $prenom = $_POST['prenom'][$i];
-        $email = $_POST['email'][$i];
-        $tel = $_POST['tel'][$i];
-        $specialite = $_POST['specialite'][$i];
+        for ($i = 0; $i < $count; $i++) {
+            $ideleves = $_POST['ideleves'][$i];
+            $nom = $_POST['nom'][$i];
+            $prenom = $_POST['prenom'][$i];
+            $email = $_POST['email'][$i];
+            $tel = $_POST['tel'][$i];
+            $specialite = $_POST['specialite'][$i];
 
-        $sql = "UPDATE eleves SET nom = ?, prenom = ?, email = ?, tel = ?, specialite = ? WHERE ideleves = ?";
-        $stmt = $connexion->prepare($sql);
+            $sql = "UPDATE eleves SET nom = ?, prenom = ?, email = ?, tel = ?, specialite = ? WHERE ideleves = ?";
+            $stmt = $connexion->prepare($sql);
 
-        if ($stmt) {
-            $stmt->bind_param("sssssi", $nom, $prenom, $email, $tel, $specialite, $ideleves);
-            $stmt->execute();
-            $stmt->close();
-        } else {
-            echo "Erreur de préparation de la requête : " . $connexion->error;
+            if ($stmt) {
+                $stmt->bind_param("sssssi", $nom, $prenom, $email, $tel, $specialite, $ideleves);
+                $stmt->execute();
+                $stmt->close();
+            } else {
+                echo "Erreur de préparation de la requête : " . $connexion->error;
+            }
         }
-    }
-    echo "L'élève a été mis à jour avec succès.";
+        echo "L'élève a été mis à jour avec succès.";
 
-    $connexion->close();
+        $connexion->close();
+    }
 }
+modifierEleves();
+
+include_once __DIR__ . '/../templates/footer.html';
 ?>
--- modifier.php
+++ modifier.php
@@ -1,24 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/base.css">
-    <link rel="stylesheet" href="css/modifier.css">
-    <link rel="stylesheet" href="css//lister.css">
-    <link rel="shortcut icon" href="img/logoonglet.png" />
-    <link rel="stylesheet" type="text/css" href="https://normandiewebschool.fr/assets/css/main.min.0754b8da326cf187918da6de8712da35f2709f482377d5598b61e7e219ce1eda.css" media="screen">
-    <link rel="stylesheet" href="https://fonts.cdnfonts.com/css/sofia-pro">
-    <title>Inscription</title>
-</head>
-<body>
-    <?php include ('methodes/search.php');?>
-    <nav class="c-navbar-light">
-        <a class="logo" href="index.php"><img class="logonws" src="img/logo.png" alt=""></a>
-        <a class="" href="index.php">Ajouter un contact</a>
-        <a class="" href="modifier.php">Modifier un contact</a>
-        <a class="" href="lister.php">Lister les contacts</a>
-    </nav>
+<?php include_once 'templates/header.php';?>
+
     <form method="post">
         <div class="search-container">
             <input type="text" name = "recherche" class="search-box" placeholder="Rechercher...">
@@ -30,13 +11,4 @@
 
     <?php afficherResultatsRecherche();?>
 
-
-
-
-    <div class="bottom-decoration">
-        <div class="red"></div>
-        <div class="blue"></div>
-        <div class="yellow"></div>
-    </div>
-</body>
-</html>
+<?php include_once 'templates/footer.html';?>
